da14d2d8208a0925bf658ecd504c08ffd50f0bdd
Brian Melgar <officialsmp305@gmail.com>
2022-10-19 16:58:27 -0700

    [FRONTEND]: Add Initial Files For Screens.

---
+++ assets/screens/DrinkScreen.js
@@ -0,0 +1,10 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+export default function DrinkScreen() {
+  return (
+    <View>
+      <Text>DrinkScreen</Text>
+    </View>
+  );
+}
