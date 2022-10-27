c585339ca55c339f955d106d6c20e66dfee3106c
Brian Melgar <officialsmp305@gmail.com>
2022-10-26 17:10:57 -0700

    [NAV/FRONTEND]: Bottom Tab Nav Styled.

---
+++ constants/ScreenHub.js
@@ -0,0 +1,9 @@
+import DrinkScreen from '../assets/screens/DrinkScreen';
+import HomeScreen from '../assets/screens/HomeScreen';
+import ProfileScreen from '../assets/screens/ProfileScreen';
+
+export const Screens = {
+  HomeScreen,
+  ProfileScreen,
+  DrinkScreen,
+};
