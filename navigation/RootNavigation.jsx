b2bba61e758e84080165b701707021f3e39e1636
Brian Melgar <officialsmp305@gmail.com>
2022-10-30 17:36:49 -0700

    [NAV]: Add New Files

--- navigation/RootNavigation.jsx
+++ navigation/RootNavigation.jsx
@@ -14,10 +14,12 @@ export default function RootNavigation() {
           headerShown: false,
         }}
       >
+        <Stack.Screen name="Login" component={Screens.LoginScreen} />
+        <Stack.Screen name="Register" component={Screens.RegisterScreen} />
         <Stack.Screen name="Tabs" component={Tabs} />
         <Stack.Screen name="Home" component={Screens.HomeScreen} />
         <Stack.Screen name="Drink" component={Screens.DrinkScreen} />
-        <Stack.Screen name="Login" component={Screens.LoginScreen} />
+        <Stack.Screen name="Forgot" component={Screens.ForgotPasswordScreen} />
         <Stack.Screen name="Profile" component={Screens.ProfileScreen} />
       </Stack.Navigator>
     </NavigationContainer>
