da0769126689e2a5d0b01ae0f1a137563c957c56
officialsmp305 <Officialsmp305@gmail.com>
2022-11-09 02:16:53 -0800

    [CONFIG]: Hook To Load Fonts.

--- hooks/useFonts.js
+++ hooks/useFonts.js
@@ -1,11 +1,12 @@
 import { useFonts } from 'expo-font';
 import MontserratBold from '../assets/fonts/MontserratBold.ttf';
 import MontserratRegular from '../assets/fonts/MontserratRegular.ttf';
-
+import MontserratMedium from '../assets/fonts/MontserratMedium.ttf';
 export const useFontLoader = () => {
   const [fontsLoaded] = useFonts({
     MontserratBold,
     MontserratRegular,
+    MontserratMedium,
   });
   return fontsLoaded;
 };
