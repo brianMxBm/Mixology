ed70d1fcadba8ad96b2741e56038daf413164700
officialsmp305 <Officialsmp305@gmail.com>
2022-11-23 15:19:19 -0800

    [FRONTNED]: Misc Screens Added

--- assets/screens/DrinkScreen.jsx
+++ assets/screens/DrinkScreen.jsx
@@ -1,5 +1,10 @@
-import { View, Text, StyleSheet } from 'react-native';
-import React from 'react';
+import { View, Text, StyleSheet, SafeAreaView, FlatList, ActivityIndicator } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import DrinkCard from '../components/DrinkCard';
+import SimpleDrinkCard from '../components/SimpleDrinkCard';
+import { db } from '../../client/firebase-config';
+import { collection, getDocs, query, where } from 'firebase/firestore';
+import colors from '../theme/colors';
 
 const styles = StyleSheet.create({
   container: {
@@ -9,10 +14,32 @@ const styles = StyleSheet.create({
   },
 });
 
-export default function DrinkScreen() {
+export default function DrinkScreen({ route }) {
+  //const [loading, setLoading] = useState(true);
+  const [drinks, setDrinks] = useState([]);
+  const { category } = route.params;
+
+  const getDrinks = async () => {
+    const queryDrink = query(collection(db, 'drinks'), where('category ', '==', category));
+    const querySnapShot = await getDocs(queryDrink);
+    querySnapShot.forEach((doc) => {
+      setDrinks(doc.data());
+    });
+  };
+
+  useEffect(() => {
+    getDrinks();
+    console.log(drinks);
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Text>DrinkScreen</Text>
-    </View>
+    <SafeAreaView style={{ flex: 1, backgroundColor: colors.white }}>
+      <FlatList
+        showsVerticalScrollIndicator={false}
+        numColumns={2}
+        data={drinks}
+        renderItem={({ item }) => <SimpleDrinkCard food={item} />}
+      />
+    </SafeAreaView>
   );
 }
