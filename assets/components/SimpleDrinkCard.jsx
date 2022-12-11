39370919536bb5c3a1d36a62004eb48ee4f8817b
officialsmp305 <Officialsmp305@gmail.com>
2022-12-10 20:47:56 -0800

    [FRONTEND]: Prototype Design For Category Flatlist

--- assets/components/SimpleDrinkCard.jsx
+++ assets/components/SimpleDrinkCard.jsx
@@ -1,30 +1,46 @@
-import React from 'react';
-import { View, Text, TouchableHighlight, Image, StyleSheet } from 'react-native';
+import React, { useContext } from 'react';
+import {
+  View,
+  Text,
+  TouchableHighlight,
+  Image,
+  StyleSheet,
+  ScrollView,
+  TouchableOpacity,
+} from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import colors from '../theme/colors';
-import { CARD_WIDTH } from '../../constants/dimensions';
+import { WIDTH } from '../../constants/dimensions';
+import Icon, { Icons } from '../theme/icons';
+import { UserContext } from '../../utils/UserContext';
 
 const styles = StyleSheet.create({
   card: {
-    height: 220, //TODO: Don't have a height like this.
-    width: CARD_WIDTH,
-    marginHorizontal: 10,
-    marginBottom: 20,
-    marginTop: 50,
-    borderRadius: 15,
-    elevation: 13,
+    height: 225,
     backgroundColor: colors.white,
-    shadowOffset: { width: 0, height: 10 },
-    shadowColor: colors.black,
+    width: WIDTH * 0.43,
+    marginHorizontal: 2,
+    borderRadius: 20,
+    shadowOffset: { width: 0, height: 8 },
+    shadowOpacity: 0.5,
     shadowRadius: 9,
-    shadowOpacity: 0.2,
     borderColor: colors.black,
-    borderWidth: 0.5,
+    borderWidth: 0.9,
+    marginBottom: 20,
+    padding: 15,
+  },
+  headerWrapper: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingHorizontal: 20,
+    paddingTop: 20,
+    alignItems: 'center',
   },
 });
 
 export default function SimpleDrinkCard({ drink }) {
   const navigation = useNavigation();
+  const user = useContext(UserContext);
   return (
     <TouchableHighlight
       underlayColor={colors.white}
@@ -33,24 +49,82 @@ export default function SimpleDrinkCard({ drink }) {
       style={{ alignItems: 'center', justifyContent: 'center', flex: 1 }}
     >
       <View style={styles.card}>
-        <View style={{ alignItems: 'center' }}>
-          <Image source={{ uri: drink.image }} style={{ height: 120, width: 120 }} />
-        </View>
-        <View style={{ marginHorizontal: 20 }}>
-          <Text ellipsizeMode="tail" numberOfLines={1} style={{ fontSize: 15, fontWeight: 'bold' }}>
-            {drink.name}
-          </Text>
+        <View styke={{ alignItems: 'flex-end' }}>
+          <View
+            style={{
+              width: 30,
+              height: 30,
+              borderRadius: 20,
+              justifyContent: 'centter',
+              alignItems: 'center',
+              backgroundColor: colors.gray,
+            }}
+          >
+            {user ? (
+              <TouchableOpacity
+                style={{
+                  borderRadius: 20,
+                  backgroundColor: colors.black,
+                  height: 30,
+                  width: 30,
+                  alignItems: 'center',
+                  justifyContent: 'center',
+                }}
+              >
+                <Icon type={Icons.FontAwesome} size={20} name={'star'} color={colors.rating} />
+              </TouchableOpacity>
+            ) : null}
+          </View>
         </View>
         <View
           style={{
-            marginTop: 10,
-            marginHorizontal: 20,
-            flexDirection: 'row',
-            justifyContent: 'space-between',
+            marginTop: 5,
+            alignItems: 'center',
           }}
         >
-          <Text style={{ fontSize: 18, fontWeight: 'bold' }}>{drink.category}</Text>
-          <View></View>
+          <Image
+            source={{ uri: drink.image }}
+            style={{ resizeMode: 'contain', height: 105, width: 100 }}
+          />
+        </View>
+        <View
+          style={{
+            justifyContent: 'center',
+          }}
+        >
+          <Text
+            ellipsizeMode="tail"
+            numberOfLines={1}
+            style={{
+              fontSize: 20,
+              fontWeight: 'bold',
+              color: colors.black,
+              fontFamily: 'Avenir-Roman',
+            }}
+          >
+            {drink.name}
+          </Text>
+
+          <View
+            style={{
+              flexDirection: 'row',
+              justifyContent: 'space-between',
+              marginTop: 5,
+            }}
+          >
+            <Text
+              ellipsizeMode="tail"
+              numberOfLines={1}
+              style={{
+                fontSize: 14,
+                fontWeight: 'bold',
+                color: colors.orange,
+                fontFamily: 'Avenir-Roman',
+              }}
+            >
+              {drink.category.toUpperCase()}
+            </Text>
+          </View>
         </View>
       </View>
     </TouchableHighlight>
--- assets/screens/DrinkCategoryScreen.jsx
+++ assets/screens/DrinkCategoryScreen.jsx
@@ -1,4 +1,12 @@
-import { View, Text, StyleSheet, SafeAreaView, FlatList, ActivityIndicator } from 'react-native';
+import {
+  View,
+  Text,
+  StyleSheet,
+  SafeAreaView,
+  FlatList,
+  ActivityIndicator,
+  Image,
+} from 'react-native';
 import React, { useState, useEffect } from 'react';
 import DrinkCard from '../components/DrinkCard';
 import SimpleDrinkCard from '../components/SimpleDrinkCard';
@@ -12,13 +20,37 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
   },
+  headerWrapper: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingHorizontal: 20,
+    alignItems: 'center',
+  },
+  drinkGreeting: {
+    fontFamily: 'Avenir-Roman',
+    fontSize: 40,
+    color: colors.black,
+    marginTop: 5,
+    fontWeight: 'light',
+  },
+  categoryGreeting: {
+    fontFamily: 'Avenir-Roman',
+    fontSize: 20,
+    color: colors.black,
+    fontWeight: 'bold',
+  },
+  icon: {
+    width: 60,
+    height: 60,
+    borderRadius: 40,
+    borderWidth: 2,
+  },
 });
 
 export default function DrinkCategoryScreen({ route }) {
   const [loading, setLoading] = useState(true);
   const [drinks, setDrinks] = useState([]);
   const { category } = route.params;
-  console.log(category.toString() + '..');
   const getDrinks = async () => {
     //TODO: Implement with redux
     const drinkData = new Array();
@@ -39,13 +71,40 @@ export default function DrinkCategoryScreen({ route }) {
 
   useEffect(() => {
     getDrinks();
-    console.log(drinks);
   }, []);
 
   return (
     <SafeAreaView style={{ flex: 1, backgroundColor: colors.white }}>
+      <View style={styles.headerWrapper}>
+        <View>
+          <Text style={styles.drinkGreeting}>How's that...</Text>
+          <Text style={styles.categoryGreeting}>{category.toUpperCase()} lookin?</Text>
+        </View>
+        <View>
+          <>
+            {category == 'brandy' ? ( //TODO: Refactor, this is horrid. String Interpolation doesn't work since images are statically indexed by the ES6?  Refer to issue:  https://github.com/facebook/react-native/issues/2481
+              <Image style={styles.icon} source={require('../images/drinkIcons/brandy.png')} />
+            ) : category == 'wine' ? (
+              <Image style={styles.icon} source={require('../images/drinkIcons/wine.png')} />
+            ) : category == 'gin' ? (
+              <Image style={styles.icon} source={require('../images/drinkIcons/gin.png')} />
+            ) : category == 'beer' ? (
+              <Image style={styles.icon} source={require('../images/drinkIcons/beer.png')} />
+            ) : category == 'vodka' ? (
+              <Image style={styles.icon} source={require('../images/drinkIcons/vodka.png')} />
+            ) : category == 'tequila' ? (
+              <Image style={styles.icon} source={require('../images/drinkIcons/tequilla.png')} />
+            ) : category == 'sake' ? (
+              <Image style={styles.icon} source={require('../images/drinkIcons/sake.png')} />
+            ) : category == 'scotch' ? (
+              <Image style={styles.icon} source={require('../images/drinkIcons/scotch.png')} />
+            ) : null}
+          </>
+        </View>
+      </View>
       <FlatList
         showsVerticalScrollIndicator={false}
+        style={{ marginTop: 20 }}
         numColumns={2}
         data={drinks}
         renderItem={({ item }) => <SimpleDrinkCard drink={item} />}
