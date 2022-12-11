7992d58ad55cda12710be31d7f3445577b09efdc
officialsmp305 <Officialsmp305@gmail.com>
2022-12-10 17:55:43 -0800

    [FRONTEND]: Settings Screen W/ Logout (TEMP)

--- assets/screens/SettingsScreen.jsx
+++ assets/screens/SettingsScreen.jsx
@@ -1,10 +1,35 @@
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 import React from 'react';
+import colors from '../theme/colors';
+import { signOut } from 'firebase/auth';
+import { auth } from '../../client/firebase-config';
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
 
-export default function SettingsScreen() {
+export default function SettingsScreen({ navigation }) {
+  const logout = () => {
+    signOut(auth)
+      .then(() => {
+        navigation.replace('Login');
+      })
+      .catch((error) => {
+        console.log(error); //TODO: Implement Better Error Handlng
+      });
+  };
+
   return (
-    <View>
-      <Text>SettingsScreen</Text>
+    <View style={styles.container}>
+      <TouchableOpacity
+        onPress={() => logout()}
+        style={{ backgroundColor: colors.orange, padding: 30 }}
+      >
+        <Text>Logout</Text>
+      </TouchableOpacity>
     </View>
   );
 }
