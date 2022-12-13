d5d7a1359724492da7139adf1df6c42e27ea8d7d
officialsmp305 <Officialsmp305@gmail.com>
2022-12-13 00:00:48 -0800

    [FRONTEND]: Rearrange Drink Screen W/ Updated Card

--- assets/components/SimpleDrinkCard.jsx
+++ assets/components/SimpleDrinkCard.jsx
@@ -36,56 +36,58 @@ const styles = StyleSheet.create({
     paddingTop: 20,
     alignItems: 'center',
   },
+  rating: {
+    fontFamily: 'Avenir-Roman',
+    fontSize: 15,
+    color: colors.textDark,
+    fontWeight: 'bold',
+  },
 });
 
 export default function SimpleDrinkCard({ drink }) {
   const navigation = useNavigation();
   const user = useContext(UserContext);
+
+  if (!drink) {
+    //TODO: Implement Loading Screen.
+    return <Text>Loading...</Text>;
+  }
+
   return (
     <TouchableHighlight
       underlayColor={colors.white}
       activeOpacity={0.8}
-      onPress={() => navigation.navigate('DetailsScreen', drink)}
+      onPress={() => navigation.navigate('Details', { drink })}
       style={{ alignItems: 'center', justifyContent: 'center', flex: 1 }}
     >
       <View style={styles.card}>
         <View styke={{ alignItems: 'flex-end' }}>
-          <View
-            style={{
-              width: 30,
-              height: 30,
-              borderRadius: 20,
-              justifyContent: 'centter',
-              alignItems: 'center',
-              backgroundColor: colors.gray,
-            }}
-          >
-            {user ? (
-              <TouchableOpacity
-                style={{
-                  borderRadius: 20,
-                  backgroundColor: colors.black,
-                  height: 30,
-                  width: 30,
-                  alignItems: 'center',
-                  justifyContent: 'center',
-                }}
-              >
-                <Icon type={Icons.FontAwesome} size={20} name={'star'} color={colors.rating} />
-              </TouchableOpacity>
-            ) : null}
-          </View>
+          {user ? (
+            <TouchableOpacity
+              style={{
+                height: 30,
+                width: 30,
+              }}
+            >
+              <Icon type={Icons.FontAwesome} size={30} name={'heart'} color={colors.orange} />
+            </TouchableOpacity>
+          ) : null}
         </View>
         <View
           style={{
             marginTop: 5,
             alignItems: 'center',
+            flexDirection: 'row',
+            justifyContent: 'center',
           }}
         >
           <Image
             source={{ uri: drink.image }}
             style={{ resizeMode: 'contain', height: 105, width: 100 }}
           />
+          <View
+            style={{ flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}
+          ></View>
         </View>
         <View
           style={{
@@ -109,7 +111,7 @@ export default function SimpleDrinkCard({ drink }) {
             style={{
               flexDirection: 'row',
               justifyContent: 'space-between',
-              marginTop: 5,
+              marginTop: 10,
             }}
           >
             <Text
@@ -118,12 +120,24 @@ export default function SimpleDrinkCard({ drink }) {
               style={{
                 fontSize: 14,
                 fontWeight: 'bold',
-                color: colors.orange,
+                color: colors.black,
                 fontFamily: 'Avenir-Roman',
               }}
             >
               {drink.category.toUpperCase()}
             </Text>
+            <View style={{ flexDirection: 'row' }}>
+              <Icon type={Icons.FontAwesome} size={15} name={'heart'} color={colors.orange} />
+              <Text style={styles.rating}>24</Text>
+              <Icon
+                style={{ marginLeft: 5 }}
+                type={Icons.FontAwesome}
+                size={15}
+                name={'star'}
+                color={colors.black}
+              />
+              <Text style={styles.rating}>54</Text>
+            </View>
           </View>
         </View>
       </View>
--- assets/screens/DrinkScreen.jsx
+++ assets/screens/DrinkScreen.jsx
@@ -1,37 +1,65 @@
-import { View, Text, SafeAreaView, FlatList } from 'react-native';
-import React from 'react';
-import DrinkCategoryHeader from '../components/DrinkHeader';
+import { View, Text, FlatList, ScrollView, StyleSheet } from 'react-native';
+import { db } from '../../client/firebase-config';
+import { doc, getDoc, limit, collection, getDocs, query } from 'firebase/firestore';
+import { UserContext } from '../../utils/UserContext';
+import React, { useContext, useState, useEffect } from 'react';
 import SearchBar from '../components/SearchBar';
-import { WIDTH } from '../../constants/dimensions';
+import SimpleDrinkCard from '../components/SimpleDrinkCard';
+
+import DrinkHeader from '../components/DrinkHeader';
 
 export default function DrinkScreen() {
+  const user = useContext(UserContext); //TODO: Repeated again. Either utilze another provider or use redux to only query once.
+  const [currUser, setCurrUser] = useState();
   const [drinks, setDrinks] = useState([]);
-  //const [loading, setLoading] = useState(true);
   const getDrinks = async () => {
     //TODO: Implement with redux
     const drinkData = new Array();
-    const queryDrink = query(collection(db, 'drinks'), where('category ', '==', category));
+    const queryDrink = query(collection(db, 'drinks'), limit(102));
     const querySnapShot = await getDocs(queryDrink);
     querySnapShot.forEach((doc) => {
-      drinkData.push(doc.data());
+      //TODO: Add paginiation
+      drinkData.push({
+        image: doc.data().image,
+        name: doc.data().name,
+        category: doc.data()['category '],
+        ingredients: doc.data().ingredients,
+        instructions: doc.data().instructions,
+      });
     });
-    console.log(drinkData);
     setDrinks(drinkData);
   };
+  const getUserDetails = async () => {
+    try {
+      const userRef = doc(db, 'users', user.uid);
+      const docSnap = await getDoc(userRef);
+      setCurrUser(docSnap.data());
+    } catch (error) {
+      console.log(error); //TODO: Implement actual error handling
+    }
+  };
+
+  useEffect(() => {
+    getUserDetails();
+    getDrinks();
+  }, []);
+
+  if (!currUser || !drinks) {
+    return <Text>..loading</Text>;
+  }
   return (
-    <SafeAreaView>
-      <DrinkCategoryHeader />
-      <View style={{ marginTop: 20 }}>
-        <SearchBar barStyle={{ width: WIDTH * 0.5 }} />
-      </View>
-      <View>
+    <View style={StyleSheet.container}>
+      <ScrollView>
+        <DrinkHeader user={currUser} />
+        <SearchBar searchScreen={true} style={{ paddingTop: 25 }} />
         <FlatList
           showsVerticalScrollIndicator={false}
+          style={{ marginTop: 20 }}
           numColumns={2}
           data={drinks}
           renderItem={({ item }) => <SimpleDrinkCard drink={item} />}
         />
-      </View>
-    </SafeAreaView>
+      </ScrollView>
+    </View>
   );
 }
